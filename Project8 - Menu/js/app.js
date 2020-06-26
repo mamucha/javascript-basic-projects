312083be9aeab2092c8b90611d10d94fa3f8be09
Marcin <marcin9213@gmail.com>
2020-06-26 20:07:39 +0200

    Project8 - Menu ->  dynamic page insert

--- Project8 - Menu/js/app.js
+++ Project8 - Menu/js/app.js
@@ -1,14 +1,142 @@
-const btnMenu = document.querySelectorAll(".btn__filter");
-const menuBox = [...document.querySelectorAll(".menu__item")];
+const menu = [
+	{
+		id: 1,
+		title: "buttermilk pancakes",
+		category: "breakfast",
+		price: 15.99,
+		img: "./images/item-1.jpeg",
+		desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+	},
+	{
+		id: 2,
+		title: "diner double",
+		category: "lunch",
+		price: 13.99,
+		img: "./images/item-2.jpeg",
+		desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+	},
+	{
+		id: 3,
+		title: "godzilla milkshake",
+		category: "shakes",
+		price: 6.99,
+		img: "./images/item-3.jpeg",
+		desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+	},
+	{
+		id: 4,
+		title: "country delight",
+		category: "breakfast",
+		price: 20.99,
+		img: "./images/item-4.jpeg",
+		desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+	},
+	{
+		id: 5,
+		title: "egg attack",
+		category: "lunch",
+		price: 22.99,
+		img: "./images/item-5.jpeg",
+		desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+	},
+	{
+		id: 6,
+		title: "oreo dream",
+		category: "shakes",
+		price: 18.99,
+		img: "./images/item-6.jpeg",
+		desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+	},
+	{
+		id: 7,
+		title: "bacon overflow",
+		category: "breakfast",
+		price: 8.99,
+		img: "./images/item-7.jpeg",
+		desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+	},
+	{
+		id: 8,
+		title: "american classic",
+		category: "lunch",
+		price: 12.99,
+		img: "./images/item-8.jpeg",
+		desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+	},
+	{
+		id: 9,
+		title: "quarantine buddy",
+		category: "shakes",
+		price: 16.99,
+		img: "./images/item-9.jpeg",
+		desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+	},
+	{
+		id: 10,
+		title: "bison steak",
+		category: "dinner",
+		price: 22.99,
+		img: "./images/item-10.jpeg",
+		desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+	},
+];
 
-btnMenu.forEach(function (element) {
-	element.addEventListener("click", function () {
-		console.log(this.dataset.id);
+// const btnMenu = document.querySelectorAll(".btn__filter");
+// const menuBox = [...document.querySelectorAll(".menu__item")];
 
-		menuBox.filter((el) =>
-			el.dataset.category != this.dataset.id && this.dataset.id != "all"
-				? el.classList.add("hide")
-				: el.classList.remove("hide")
-		);
-	});
+// btnMenu.forEach(function (element) {
+// 	element.addEventListener("click", function () {
+// 		console.log(this.dataset.id);
+
+// 		menuBox.filter((el) =>
+// 			el.dataset.category != this.dataset.id && this.dataset.id != "all"
+// 				? el.classList.add("hide")
+// 				: el.classList.remove("hide")
+// 		);
+// 	});
+// });
+
+//--- dynamic page insert
+
+window.addEventListener("DOMContentLoaded", function () {
+	displayMenuItems(menu);
+	displayMenuButtons();
 });
+
+const displayMenuItems = (menuItems) => {
+	const mainMenu = document.querySelector(".main__menu");
+
+	let displayMenu = menuItems.map(function (item) {
+		return `<article class="menu__item" data-category="${item.category}"> 
+		<div class="menu__img"> <img src="${item.img}" alt="${item.title}"> </div>
+		<div class="menu__info">
+		<header class="menu__title">
+		<h3>${item.title}</h3>
+		<span class="price">${item.price}</span>
+		</header>
+		<p>${item.desc}</p>
+		</div>
+		</article> `;
+	});
+	mainMenu.innerHTML = displayMenu.join("");
+};
+
+const displayMenuButtons = () => {
+	const btnMenu = document.querySelectorAll(".btn__filter");
+
+	btnMenu.forEach(function (element) {
+		element.addEventListener("click", function () {
+			const buttonCategory = this.dataset.id;
+
+			const category = menu.filter(function (menuItem) {
+				if (menuItem.category === buttonCategory) return menuItem;
+			});
+
+			if (buttonCategory === "all") {
+				displayMenuItems(menu);
+			} else {
+				displayMenuItems(category);
+			}
+		});
+	});
+};
