5d0ceaeb64f7d7d9982f00f7d283595f212a37f3
Marcin <marcin9213@gmail.com>
2020-07-05 12:04:26 +0200

    add style

--- Project14 - ToDoList/js/app.js
+++ Project14 - ToDoList/js/app.js
@@ -3,9 +3,25 @@ const inputValue = document.querySelector("#list");
 const itemList = document.querySelector(".item__list");
 const itemEdit = document.querySelector(".item__edit");
 const itemDelete = document.querySelector(".item__delete");
+const itemClearBtn = document.querySelector(".item__clear");
+const alertText = document.querySelector(".main__title h3");
 
 let editFlag = false;
 let editElement;
+
+const infoTask = (text, item, action) => {
+	alertText.style.display = "block";
+	alertText.textContent = `${text} ${item}`;
+
+	setTimeout(() => {
+		alertText.style.display = "none";
+	}, 1000);
+};
+
+const clearTask = () => {
+	document.querySelectorAll(".item__wrapper").forEach((el) => el.remove());
+	itemClearBtn.style.display = "none";
+};
 
 const editTask = (e) => {
 	editFlag = true;
@@ -17,11 +33,20 @@ const editTask = (e) => {
 
 const removeTask = (e) => {
 	e.target.parentElement.parentElement.parentNode.remove();
+	if (document.querySelectorAll(".item__wrapper").length == 0)
+		itemClearBtn.style.display = "none";
+
+	infoTask(
+		"item delete",
+		e.target.parentElement.parentElement.parentNode.firstElementChild
+			.textContent
+	);
 };
 
 const addTask = (e) => {
 	e.preventDefault();
 	const taskValue = inputValue.value;
+	itemClearBtn.style.display = "flex";
 
 	if (taskValue !== "" && !editFlag) {
 		const taskBox = document.createElement("div");
@@ -29,6 +54,9 @@ const addTask = (e) => {
 		taskBox.innerHTML = `<p>${taskValue}</p><div class="item__change"><span class="item__edit"><i class="fas fa-edit"></i></span><span class="item__delete"><i class="fas fa-trash"></i></span>`;
 		inputValue.value = "";
 		itemList.appendChild(taskBox);
+
+		infoTask("item added to the list", taskValue);
+
 		document
 			.querySelectorAll(".item__delete")
 			.forEach((el) => el.addEventListener("click", removeTask));
@@ -36,8 +64,9 @@ const addTask = (e) => {
 		document
 			.querySelectorAll(".item__edit")
 			.forEach((el) => el.addEventListener("click", editTask));
+
+		document.querySelector(".item__btn").addEventListener("click", clearTask);
 	} else if (editFlag) {
-		console.log(editElement);
 		editElement.textContent = inputValue.value;
 		inputValue.value = "";
 		editFlag = false;
